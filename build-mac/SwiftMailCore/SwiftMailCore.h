43027b4cf2a8bd868344c1ec44cfc4d85d0909ab
Gennaro Stanzione <gst@bit4id.com>
2019-08-30 15:07:21 +0200

    Aggiunto aggregate per la libreria SwiftMailCore

---
+++ build-mac/SwiftMailCore/SwiftMailCore.h
@@ -0,0 +1,19 @@
+//
+//  SwiftMailCore.h
+//  SwiftMailCore
+//
+//  Created by Gennaro Stanzione on 30/08/2019.
+//  Copyright © 2019 MailCore. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SwiftMailCore.
+FOUNDATION_EXPORT double SwiftMailCoreVersionNumber;
+
+//! Project version string for SwiftMailCore.
+FOUNDATION_EXPORT const unsigned char SwiftMailCoreVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftMailCore/PublicHeader.h>
+
+
---
+++ build-mac/test-swift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  test-swift
+//
+//  Created by Gennaro Stanzione on 30/08/2019.
+//  Copyright © 2019 MailCore. All rights reserved.
+//
+
+import UIKit
+import MailCore
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        let imap = MCOIMAPSession()
+        
+        
+    }
+
+
+}
+
